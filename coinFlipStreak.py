14e3d11e43c1c95761b33a424534bfc2a05e2cac
NebulaScout <144537520+NebulaScout@users.noreply.github.com>
2023-11-04 08:51:38 +0300

    Update coinFlipStreak.py

--- coinFlipStreak.py
+++ coinFlipStreak.py
@@ -1,4 +1,5 @@
-# This is a program that randomizes a coin flip and checks how many times a streak of 6 heads or 6 tails occurs in a row and records it after every 100 coin flips for 10000 times and gives a percentage of how many times a streak would occur.
+# This is a program that randomizes a coin flip and checks how many times a streak of 6 heads or 6 tails occurs in a row and 
+# records it after every 100 coin flips for 10000 times and gives a percentage of how many times a streak would occur.
 import random
 
 numberOfStreaks = 0
